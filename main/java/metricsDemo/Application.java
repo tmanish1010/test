6b489102404f5aea6d3515590fdf1affe603c6ea
manish.rajput <tmanish1028@gmail.com>
2023-03-30 00:31:09 +0530

    DEVOPS-10077

---
+++ main/java/metricsDemo/Application.java
@@ -0,0 +1,17 @@
+package metricsDemo;
+
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
+public class Application {
+
+    public static void main(String[] args) {
+	            SpringApplication.run(Application.class, args);
+	System.out.println("HELLO");
+    }
+}
